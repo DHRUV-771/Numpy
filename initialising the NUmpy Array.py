8a52a07e3073696937ff27efa0fa15a2eb36deb5
DHRUV-771 <dhruvchoudhary3777@gmail.com>
2025-12-15 17:09:26 +0530

    Initialize NumPy array operations script
    Initial implementation of a script to demonstrate basic NumPy array operations, including 1D, 2D, and 3D arrays.

---
+++ initialising the NUmpy Array.py
@@ -0,0 +1,18 @@
+# this is thye starting of the numpy array
+import numpy as np
+a=np.array([10,20,30,40,50])
+b=a*2
+print(b)
+print(type(b))
+
+#2-Dimensional Numpy Array
+c=np.array([[1,2,3],
+           [4,5,6]])
+d=c/2
+print(d)
+#3-Dimensional Numpy Array
+E=np.array([[1,2,3],
+           [4,5,6],
+           [7,8,9]])
+f=E
+print(f)
